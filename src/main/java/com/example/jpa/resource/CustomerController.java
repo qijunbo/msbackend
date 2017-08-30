c7a0568cb52428493169af19155eb7fa7d3b37fa
LAPTOP-MTIQEU28\qijunbo <junboqi@foxmail.com>
2017-08-30 17:01:14 +0800

    add HTTP  Delete with  requestbody

--- src/main/java/com/example/jpa/resource/CustomerController.java
+++ src/main/java/com/example/jpa/resource/CustomerController.java
@@ -5,6 +5,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -69,5 +71,15 @@ public class CustomerController {
 		return null;
 	}
 
-	 
+    @RequestMapping(method = DELETE)
+    public @ResponseBody int delete(@RequestBody List<Customer> customers) {
+
+        if (customers != null) {
+            customers.forEach(c -> repository.delete(c.getId()));
+            return customers.size();
+        } else {
+            return 0;
+        }
+
+    }
 }
